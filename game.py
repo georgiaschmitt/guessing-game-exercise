8d482b5ad021bc136fa284870d84a43e2f8e2cfc
vagrant <vagrant@vagrant.vm>
2020-09-29 21:31:10 +0000

    Constructed conditionals and guess feedback

--- game.py
+++ game.py
@@ -6,3 +6,16 @@ print('Hello new player!')
 player_name = input("What is your name?")
 secret_number = random.randint(1, 100)
 # print(secret_number)
+players_guess = 0
+guesscount = 0
+
+while players_guess != secret_number: 
+    players_guess = int(input("What's your guess?"))
+    guesscount += 1
+    if players_guess < secret_number:
+        print("Guess higher!")
+    elif players_guess > secret_number:
+        print("Guess lower!")
+else: 
+    print(f"You got it! It took you {guesscount} guesses!")
+
