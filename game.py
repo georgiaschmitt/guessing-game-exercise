c1ca35ffd284607d26c2f55df2467f732d99bbad
vagrant <vagrant@vagrant.vm>
2020-09-29 21:08:42 +0000

    added function to generate secret number

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 """A number-guessing game."""
+import random
 
 # Put your code here
 print('Hello new player!')
 player_name = input("What is your name?")
+secret_number = random.randint(1, 100)
+# print(secret_number)
